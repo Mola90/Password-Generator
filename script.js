67d80520f758193c94ca22b2ba8eb591fbb68ed9
Mola Dafa <mola_d@hotmail.com>
2024-01-15 03:22:18 +1100

    variables set up

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 // Assignment code here
+var length = 0;
+var lowerCase = false;
+var lowerCase = false;
+var numbers = false;
+var specialChar = false;
 
+var charOptions = "abcdefghijklmnopqrstuvwxyz";
+var numberOptions = "0123456789"
+var specialOptions = "!\"#$%&'()*+,-./:;<=>?@[]^_`{|}~\\"
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -15,3 +23,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
