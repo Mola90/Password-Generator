d427d1c74a546a1ca5e8d123740716e86d76a67c
Mola Dafa <mola_d@hotmail.com>
2024-01-15 05:46:14 +1100

    shuffling added

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ var lowerCase = false;
 var upperCase = false;
 var numbers = false;
 var specialChar = false;
+var acceptableLenght = false;
+var password = " ";
 
 var charOptions = "abcdefghijklmnopqrstuvwxyz";
 var numberOptions = "0123456789";
@@ -14,6 +16,66 @@ lowerCase = window.confirm("Would you like to include lower case letters?");
 upperCase = window.confirm("Would you like to include Uppercase letters?");
 numbers = window.confirm("Would you like your password to include numbers?");
 specialChar= window.confirm("Would you like your password to include special characters?");
+
+var checklength = function(){
+if (length => 8 && length <= 128)
+    {
+      acceptableLenght = true;
+
+    }
+else
+    {
+      window.alert("You have not chosen an accpetable password length");
+      length = window.prompt("Warning! you have not chosen a an acceptable password. Enter a number beteen 8 and 128");
+      checklength();
+
+
+    }};
+
+
+if (lowerCase || upperCase || numbers || specialChar && acceptableLenght)
+{
+//generate password
+}
+
+
+function shuffleArray(array) {
+  return array.sort(function() {
+       return Math.random() - 0.5;
+  })};
+
+
+function generatePassword()
+  {
+    if(lowerCase)
+    {
+      password = password + charOptions;
+    }
+
+    if(upperCase)
+    { 
+      password = password + charOptions.toUpperCase(); 
+    }
+
+    if(numbers)
+    {
+      password = password + numberOptions;
+    }
+
+    if(specialChar)
+    {
+      password = password + specialOptions;
+    }   
+
+    passwordArray = password.split("");
+
+    console.log(shuffleArray(passwordArray));
+
+    
+  }
+
+  generatePassword();
+
 
 
 // Get references to the #generate element
